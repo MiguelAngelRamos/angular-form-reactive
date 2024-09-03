f2c88349250d16e5b5a29416e9abaa6a7dc8ebf9
Miguel Ramos <return.infox@gmail.com>
2024-09-03 12:00:30 -0400

    formularios reactivos

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+  constructor( private formBuilder: FormBuilder){}
+}
