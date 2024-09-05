4cbaa1e7b07e39e5fbdd9e6f5dd9d8dc33f7a7c6
Miguel Ramos <return.infox@gmail.com>
2024-09-05 09:54:35 -0400

    actualizacion del formulario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  loginForm!: FormGroup;
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup; // su formulario
+
   constructor( private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email] ],
+      password: ['', Validators.required]
+      
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.valid);
+    
+  }
+
 }
